b2873441e0e3d0e330059ccb56190406966d775b
huaixiaoz <hello@itmp.top>
2016-10-15 17:39:24 +0800

    presuffix

---
+++ projs/strings/suffix/presuffix.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    var str string = "Thisis an example of a string"
+    fmt.Printf("T/F? Does the string \"%s\" has prefix %s?", str, "Th")
+    fmt.Printf("%t\n", strings.HasPrefix(str, "Th"))
+}
