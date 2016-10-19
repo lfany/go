24975de0c2bedd50c41faf7a31678ea074bf1f1d
hello <hello@itmp.top>
2016-10-19 17:40:47 +0800

    add

---
+++ xx/readinput2.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+import "bufio"
+import "os"
+
+var (
+	inputReader *bufio.Reader
+	input string
+	err error
+)
+
+func main() {
+	inputReader = bufio.NewReader(os.Stdin)
+	fmt.Println("Enter:")
+	input, err = inputReader.ReadString('q')
+	if err == nil {
+		fmt.Printf("The input was: %s\n", input)
+	}
+}
