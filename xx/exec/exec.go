e8b0b26638b59854baffea4d62b05342dcb9755c
hello <hello@itmp.top>
2016-10-20 17:28:40 +0800

    exec

---
+++ xx/exec/exec.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+func main() {
+	env := os.Environ()
+	procAttr := os.ProcAttr{
+		Env: env,
+		Files:[]*os.File{
+			os.Stdin,
+			os.Stdout,
+			os.Stderr,
+		},
+	}
+
+	pid, err := os.StartProcess("/usr/bin/ls", []string{"ls", "-l"}, &procAttr)
+	if (err != nil) {
+		fmt.Printf("%v", err)
+		os.Exit(1)
+	}
+	fmt.Printf("The process id is %v", pid)
+}
