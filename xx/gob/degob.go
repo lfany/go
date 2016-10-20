960a9023592545e5fce317629c1c74c510b04e8e
hello <hello@itmp.top>
2016-10-20 16:31:06 +0800

    update degob

---
+++ xx/gob/degob.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"os"
+	"bufio"
+	"encoding/gob"
+	"log"
+	"fmt"
+)
+
+type Address struct {
+	Type 	string
+	City	string
+	Country	string
+}
+
+type VCard struct {
+	FirstName	string
+	LastName	string
+	Address		[]*Address
+	Remark 		string
+}
+
+var (
+	content string
+	vc VCard
+)
+
+func main() {
+	file, _ := os.Open("vcard.gob")
+	defer file.Close()
+
+	inReader := bufio.NewReader(file)
+	dec := gob.NewDecoder(inReader)
+	err := dec.Decode(&vc)
+	if (err != nil) {
+		log.Println("Err in gob")
+	}
+	fmt.Println(vc)
+	ll := vc.Address
+
+	for i := 0; i < len(ll); i++ {
+		fmt.Println(*ll[i])
+	}
+	fmt.Println(vc.Address)
+
+}
