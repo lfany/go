e6468eaa32c2d789b9e855dcbf22f71575fe3b7c
hello <hello@itmp.top>
2016-10-21 15:21:37 +0800

    update

---
+++ xx/web/dial.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "192.168.71.66:66")
+	checkConnection(conn, err)
+	conn, err = net.Dial("udp", "192.168.71.66:66")
+	checkConnection(conn, err)
+	conn, err = net.Dial("tcp", "192.168.71.66:66")
+	checkConnection(conn, err)
+}
+func checkConnection(conn net.Conn, i error) {
+	if i != nil {
+		fmt.Printf("error %v connecting", i)
+		os.Exit(1)
+	}
+	fmt.Printf("conn : %v\n", conn)
+}
---
+++ xx/web/socket.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net"
+	"io"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var (
+		host	= "www.baidu.com"
+		port	= "80"
+		remote	= host + ":" + port
+		msg string = "GET / \n"
+		data	= make([]uint8, 4096)
+		read	= true
+		count	= 0
+	)
+
+	conn, err := net.Dial("tcp", remote)
+	if err != nil {
+		fmt.Printf("err %v\n", err)
+		os.Exit(1)
+	}
+	io.WriteString(conn, msg)
+
+	for ;read ;  {
+		count, err = conn.Read(data)
+		read = (err == nil)
+		fmt.Printf(string(data[0:count]))
+	}
+
+	conn.Close()
+}
