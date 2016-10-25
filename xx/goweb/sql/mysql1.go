74a079732718f8064baa51a86b644f2d29ffc71d
hello <hello@itmp.top>
2016-10-25 11:57:09 +0800

    insert delete update select

--- xx/goweb/sql/mysql1.go
+++ xx/goweb/sql/mysql1.go
@@ -7,6 +7,11 @@ import (
 )
 
 func main() {
+
+	//user@unix(/path/to/socket)/dbname?charset=utf8
+	//user:password@tcp(localhost:5555)/dbname?charset=utf8
+	//user:password@/dbname
+	//user:password@tcp([de:ad:be:ef::ca:fe]:80)/dbname
 	db, err := sql.Open("mysql", "root:00@/test?charset=utf8")
 	checkErr(err)
 
@@ -21,7 +26,45 @@ func main() {
 
 	fmt.Println(id)
 
-	
+//	更新数据
+	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec("astaxieupdate", id)
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+//	查询数据
+	rows, err := db.Query("SELECT * FROM userinfo")
+	checkErr(err)
+
+	for rows.Next() {
+		var uid int
+		var username string
+		var department string
+		var created string
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println(uid, "\t", username, "\t", department, "\t", created)
+	}
+
+//	删除数据
+
+	stmt, err = db.Prepare("delete from userinfo where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec(id)
+	checkErr(err)
+
+	affect, err = res.RowsAffected()
+	fmt.Println(affect)
+
+	db.Close()
+
 }
 func checkErr(err error) {
 	if err != nil {
