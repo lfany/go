3f8bb26a4ef36fff367e5d6c32e5d8f77fc50a1f
hello <hello@itmp.top>
2016-10-26 13:41:59 +0800

    xml

---
+++ xx/goweb/xml/makexml.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"os"
+)
+
+type Servers struct {
+	XMLName xml.Name	`xml:"servers"`
+	Version	string		`xml:"version,attr"`
+	Svs	[]server	`xml:"server"`
+}
+
+type server struct {
+	ServerName	string	`xml:"serverName"`
+	ServerIP	string	`xml:"serverIP"`
+}
+
+func main() {
+	v := &Servers{Version:"1"}
+	v.Svs = append(v.Svs, server{"ABC", "172.17.0.1"})
+	v.Svs = append(v.Svs, server{"ZXC", "172.17.0.2"})
+	output, err := xml.MarshalIndent(v, "", "	")
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+
+	os.Stdout.Write([]byte(xml.Header))
+	os.Stdout.Write(output)
+}
