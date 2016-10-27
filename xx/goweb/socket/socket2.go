6586d4211384c075a7c5f5a3e44f62de82c75f10
hello <hello@itmp.top>
2016-10-27 15:46:31 +0800

    socket time

--- xx/goweb/socket/socket2.go
+++ xx/goweb/socket/socket2.go
@@ -4,25 +4,47 @@ import (
 	"os"
 	"fmt"
 	"net"
-	"io/ioutil"
 )
 
 func main() {
+	var service string
+
 	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s host:port\n", os.Args[0])
-		os.Exit(1)
+		//fmt.Fprintf(os.Stderr, "Usage: %s host:port\n", os.Args[0])
+		//os.Exit(1)
+		service = "127.0.0.1:1200"
+	} else {
+		service = os.Args[1]
 	}
-	service := os.Args[1]
+
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
 	checkErr(err)
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	checkErr(err)
-	_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
+	//_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
+	_, err = conn.Write([]byte("timestamp"))
 	checkErr(err)
-	result, err := ioutil.ReadAll(conn)
-	checkErr(err)
-	fmt.Println(string(result))
+	//result, err := ioutil.ReadAll(conn)
+	//checkErr(err)
+	//fmt.Println(string(result))
+
+	readServer(conn)
 	os.Exit(0)
+
+}
+func readServer(conn *net.TCPConn) {
+	defer conn.Close()
+	result := make([]byte, 128)
+	readLen, err := conn.Read(result)
+	checkErr(err)
+	fmt.Println(string(result), "##", readLen)
+
+	if readLen == 0 {
+		fmt.Println("Server Closed")
+	} else {
+		fmt.Println("xxx:", string(result[:readLen]), "xxx")
+	}
+	result = make([]byte, 128)
 }
 func checkErr(err error) {
 	if err != nil {
--- xx/goweb/socket/socket_time2.go
+++ xx/goweb/socket/socket_time2.go
@@ -4,6 +4,8 @@ import (
 	"net"
 	"fmt"
 	"time"
+	"strings"
+	"strconv"
 )
 
 func main() {
@@ -23,9 +25,33 @@ func main() {
 	}
 }
 func handleClient(conn net.Conn) {
-	//defer conn.Close()
-	daytime := time.Now().String()
-	conn.Write([]byte(daytime))
+	conn.SetReadDeadline(time.Now().Add(2 * time.Minute)) // set 2 minutes timeout
+	request := make([]byte, 128)
+	defer conn.Close()
+	for {
+		readLen, err := conn.Read(request)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			break
+		}
+
+		fmt.Println(string(request))
+
+		if readLen == 0 {
+			fmt.Println("##:" + string(readLen))
+			break // connection already closed by client
+		}else if strings.TrimSpace(string(request[:readLen])) == "timestamp" {
+			daytime := strconv.FormatInt(time.Now().Unix(), 10)
+			conn.Write([]byte(daytime))
+		}else {
+			daytime := time.Now().String()
+			conn.Write([]byte(daytime))
+		}
+	}
+	request = make([]byte, 128) // clear last read content
+	//daytime := time.Now().String()
+	//conn.Write([]byte(daytime))
 }
 func checkE(err error) {
 	if err != nil {
